15bc46371459cd925d6fd47dc0ff76ece69c9e79
Denise Case <denisecase@gmail.com>
2025-10-04 10:29:39 -0500

    consumers/sqlite_consumer_case - add missing parens

--- consumers/sqlite_consumer_case.py
+++ consumers/sqlite_consumer_case.py
@@ -152,7 +152,8 @@ def main():
     logger.info("Starting db testing.")
 
     # Use config to make a path to a parallel test database
-    DATA_PATH: pathlib.path = config.get_base_data_path
+    # OCT 2025: ADD parens when we call the function get_base_data_path
+    DATA_PATH: pathlib.path = config.get_base_data_path()
     TEST_DB_PATH: pathlib.Path = DATA_PATH / "test_buzz.sqlite"
 
     # Initialize the SQLite database by passing in the path
